5a46d4edbbebedf74ca51511227c6c600abd583f
Kyle <java_kwon@fcacademy.co.kr>
2021-06-23 15:49:16 +0900

    9-02. JPA Enum 형태의 값 관리 - 2

--- src/main/java/com/example/study/model/entity/OrderGroup.java
+++ src/main/java/com/example/study/model/entity/OrderGroup.java
@@ -3,14 +3,9 @@ package com.example.study.model.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+
+import com.example.study.model.enumclass.OrderType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,7 +34,8 @@ public class OrderGroup {
 
   private String status;
 
-  private String orderType;
+  @Enumerated(EnumType.STRING)
+  private OrderType orderType;
 
   private String revAddress;
 
